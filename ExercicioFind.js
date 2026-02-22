d143330b4b78ef037daf909d1b481fa07be4415b
Felipe Polegato <felipepolegatofr@gmail.com>
2026-02-22 09:51:41 -0300

    Exercicios de Find e Map + Desafio do carrinho

---
+++ ExercicioFind.js
@@ -0,0 +1,5 @@
+const filmes = ["Avatar", "Batman", "Vingadores", "Matrix", "Barbie"];
+
+const primeiroComB = filmes.find(filme => filme.startsWith("B"));
+
+console.log(primeiroComB);
---
+++ ExercicioMap.js
@@ -0,0 +1,5 @@
+const temperaturas = [20, 25, 30, 15];
+
+const temperaturasFahrenheit = temperaturas.map(celsius => celsius * 1.8 + 32);
+
+console.log(temperaturasFahrenheit);
